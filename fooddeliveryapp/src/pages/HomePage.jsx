3b806bf7a6a622f51047a23d1e31701f991fbf46
Sushanth-Personal <sushanthp.calicut@gmail.com>
2024-11-22 19:06:54 +0530

    Add: HeaderDesktopComponent with design and responsiveness

--- fooddeliveryapp/src/pages/HomePage.jsx
+++ fooddeliveryapp/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+import styles from "./styles/homepage.module.css";
+import HeaderDesktop from "../components/HeaderDesktop/HeaderDesktop";
 const HomePage = () => {
+  // const navigate = useNavigate();
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/login");
-  }, [navigate]);
- 
-  return <div>HomePage</div>;
+  return (
+    <section className={styles.homePage}>
+      <header>
+        <HeaderDesktop />
+      </header>
+      <div className={styles.navBar}>
+        
+      </div>
+      <div className={styles.searchContainer}></div>
+      <div className={styles.exclusiveDealsContainer}></div>
+      <div className={styles.categoryContainer}></div>
+      <div className={styles.popularRestaurants}></div>
+      <div className={styles.adContainer}></div>
+      <div className={styles.signUpContainer}></div>
+      <div className={styles.aboutUsContainer}></div>
+      <div className={styles.statisticsContainer}></div>
+      <footer>{/* <FooterComponent /> */}</footer>
+    </section>
+  );
 };
 
 export default HomePage;
